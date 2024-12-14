05c0722f15a6fc048dc9295775033084d254b19f
qzhhhi <zihanqin2048@gmail.com>
2024-12-14 12:40:47 +0000

    Fixed an issue with the `Imu` class not behaving correctly - The `Imu` class has been renamed to `Bmi088` for improved naming accuracy

--- librmcs/device/bmi088.hpp
+++ librmcs/device/bmi088.hpp
@@ -7,9 +7,9 @@
 
 namespace librmcs::device {
 
-class Imu {
+class Bmi088 {
 public:
-    explicit Imu(
+    explicit Bmi088(
         double sample_freq, double kp, double ki, double q0 = 1, double q1 = 0, double q2 = 0,
         double q3 = 0)
         : inv_sample_freq_(1.0 / sample_freq)
@@ -40,7 +40,7 @@ public:
         gx_ = solve_gyro(gyro.x), gy_ = solve_gyro(gyro.y), gz_ = solve_gyro(gyro.z);
         ax_ = solve_acc(acc.x), ay_ = solve_acc(acc.y), az_ = solve_acc(acc.z);
 
-        mahony_ahrs_update_imu();
+        mahony_ahrs_update_imu(ax_, ay_, az_, gx_, gy_, gz_);
     }
 
     double ax() const { return ax_; }
@@ -57,7 +57,7 @@ public:
     double q3() const { return q3_; }
 
 private:
-    void mahony_ahrs_update_imu() {
+    void mahony_ahrs_update_imu(double ax, double ay, double az, double gx, double gy, double gz) {
         // Madgwick's implementation of Mayhony's AHRS algorithm.
         // See: http://www.x-io.co.uk/node/8#open_source_ahrs_and_imu_algorithms
 
@@ -68,13 +68,13 @@ private:
 
         // Compute feedback only if accelerometer measurement valid (avoids NaN in accelerometer
         // normalization)
-        if (!((ax_ == 0.0) && (ay_ == 0.0) && (az_ == 0.0))) {
+        if (!((ax == 0.0) && (ay == 0.0) && (az == 0.0))) {
 
             // Normalize accelerometer measurement
-            recip_norm = 1 / std::sqrt(ax_ * ax_ + ay_ * ay_ + az_ * az_);
-            ax_ *= recip_norm;
-            ay_ *= recip_norm;
-            az_ *= recip_norm;
+            recip_norm = 1 / std::sqrt(ax * ax + ay * ay + az * az);
+            ax *= recip_norm;
+            ay *= recip_norm;
+            az *= recip_norm;
 
             // Estimated direction of gravity and vector perpendicular to magnetic flux
             halfvx = q1_ * q3_ - q0_ * q2_;
@@ -82,9 +82,9 @@ private:
             halfvz = q0_ * q0_ - 0.5 + q3_ * q3_;
 
             // Error is sum of cross product between estimated and measured direction of gravity
-            halfex = ay_ * halfvz - az_ * halfvy;
-            halfey = az_ * halfvx - ax_ * halfvz;
-            halfez = ax_ * halfvy - ay_ * halfvx;
+            halfex = ay * halfvz - az * halfvy;
+            halfey = az * halfvx - ax * halfvz;
+            halfez = ax * halfvy - ay * halfvx;
 
             // Compute and apply integral feedback if enabled
             if (double_ki_ > 0.0) {
@@ -93,9 +93,9 @@ private:
                 integral_fby_ += double_ki_ * halfey * (inv_sample_freq_);
                 integral_fbz_ += double_ki_ * halfez * (inv_sample_freq_);
                 // apply integral feedback
-                gx_ += integral_fbx_;
-                gy_ += integral_fby_;
-                gz_ += integral_fbz_;
+                gx += integral_fbx_;
+                gy += integral_fby_;
+                gz += integral_fbz_;
             } else {
                 // prevent integral windup
                 integral_fbx_ = 0.0;
@@ -104,22 +104,22 @@ private:
             }
 
             // Apply proportional feedback
-            gx_ += double_kp_ * halfex;
-            gy_ += double_kp_ * halfey;
-            gz_ += double_kp_ * halfez;
+            gx += double_kp_ * halfex;
+            gy += double_kp_ * halfey;
+            gz += double_kp_ * halfez;
         }
 
         // Integrate rate of change of quaternion
-        gx_ *= (0.5 * (inv_sample_freq_)); // pre-multiply common factors
-        gy_ *= (0.5 * (inv_sample_freq_));
-        gz_ *= (0.5 * (inv_sample_freq_));
+        gx *= (0.5 * (inv_sample_freq_)); // pre-multiply common factors
+        gy *= (0.5 * (inv_sample_freq_));
+        gz *= (0.5 * (inv_sample_freq_));
         qa = q0_;
         qb = q1_;
         qc = q2_;
-        q0_ += (-qb * gx_ - qc * gy_ - q3_ * gz_);
-        q1_ += (qa * gx_ + qc * gz_ - q3_ * gy_);
-        q2_ += (qa * gy_ - qb * gz_ + q3_ * gx_);
-        q3_ += (qa * gz_ + qb * gy_ - qc * gx_);
+        q0_ += (-qb * gx - qc * gy - q3_ * gz);
+        q1_ += (qa * gx + qc * gz - q3_ * gy);
+        q2_ += (qa * gy - qb * gz + q3_ * gx);
+        q3_ += (qa * gz + qb * gy - qc * gx);
 
         // Normalize quaternion
         recip_norm = 1 / std::sqrt(q0_ * q0_ + q1_ * q1_ + q2_ * q2_ + q3_ * q3_);
