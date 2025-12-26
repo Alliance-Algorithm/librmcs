847ee65af3135179e2eb6bd30ac1e19d106187e9
qzhhhi <zihanqin2048@gmail.com>
2025-12-26 06:42:59 +0000

    feat(utility): Implement assert functionality and add Immovable/Uncopyable classes

---
+++ core/src/utility/assert.inl.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+// IWYU pragma: private, include "core/src/utility/assert.hpp"
+
+#include <source_location>
+
+#ifdef NDEBUG
+# include <utility>
+#endif
+
+namespace librmcs::core::utility {
+
+[[noreturn]] void assert_func(const std::source_location& location);
+
+constexpr inline void
+    assert_failed_always(const std::source_location& location = std::source_location::current()) {
+    assert_func(location);
+}
+
+[[noreturn]] inline void
+    assert_failed(const std::source_location& location = std::source_location::current()) {
+#ifdef NDEBUG
+    (void)location;
+    std::unreachable();
+#else
+    assert_func(location);
+#endif
+}
+
+constexpr inline void assert_always(
+    bool condition, const std::source_location& location = std::source_location::current()) {
+    if (!condition) [[unlikely]]
+        assert_func(location);
+}
+
+constexpr inline void
+    assert(bool condition, const std::source_location& location = std::source_location::current()) {
+#ifdef NDEBUG
+    [[assume(condition)]];
+    (void)location;
+#else
+    assert_always(condition, location);
+#endif
+}
+
+} // namespace librmcs::core::utility
