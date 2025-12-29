eca12fea0eca9fe8fa1e07f4bce59458b5725e81
qzhhhi <zihanqin2048@gmail.com>
2025-12-29 09:20:45 +0000

    feat(utility): Add Immovable and Uncopyable helper classes

---
+++ core/src/utility/immovable.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace librmcs::core::utility {
+
+class Immovable {
+public:
+    Immovable() = default;
+    Immovable(const Immovable&) = delete;
+    Immovable& operator=(const Immovable&) = delete;
+    Immovable(Immovable&&) = delete;
+    Immovable& operator=(Immovable&&) = delete;
+};
+
+} // namespace librmcs::core::utility
---
+++ core/src/utility/uncopyable.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace librmcs::core::utility {
+
+class Uncopyable {
+public:
+    Uncopyable() = default;
+    Uncopyable(const Uncopyable&) = delete;
+    Uncopyable& operator=(const Uncopyable&) = delete;
+    Uncopyable(Uncopyable&&) = default;
+    Uncopyable& operator=(Uncopyable&&) = default;
+};
+
+} // namespace librmcs::core::utility
