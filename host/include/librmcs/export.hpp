8eeb25782b687c5af1b1d3112e8649c9c2d7de1e
qzhhhi <zihanqin2048@gmail.com>
2026-03-03 08:33:35 +0000

    fix

--- host/include/librmcs/export.hpp
+++ host/include/librmcs/export.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
 #if defined(_MSC_VER)
-    #ifdef BUILDING_LIBRMCS
-        #define LIBRMCS_API __declspec(dllexport)
-    #elif defined(STATIC_LINKING_LIBRMCS)
-        #define LIBRMCS_API
-    #else
-        #define LIBRMCS_API __declspec(dllimport)
-    #endif
+# ifdef BUILDING_LIBRMCS
+#  define LIBRMCS_API __declspec(dllexport)
+# elif defined(STATIC_LINKING_LIBRMCS)
+#  define LIBRMCS_API
+# else
+#  define LIBRMCS_API __declspec(dllimport)
+# endif
 #elif defined(__GNUC__) || defined(__clang__)
-    #ifdef BUILDING_LIBRMCS
-        #define LIBRMCS_API __attribute__((visibility("default")))
-    #else
-        #define LIBRMCS_API
-    #endif
+# ifdef BUILDING_LIBRMCS
+#  define LIBRMCS_API __attribute__((visibility("default")))
+# else
+#  define LIBRMCS_API
+# endif
 #else
-    #define LIBRMCS_API
+# define LIBRMCS_API
 #endif
