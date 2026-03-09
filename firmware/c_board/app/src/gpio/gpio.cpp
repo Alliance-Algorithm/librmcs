1b3b6a0f83a4a663c7969fdd4341a2669346389a
qzhhhi <zihanqin2048@gmail.com>
2026-03-09 15:07:26 +0000

    fix

--- firmware/c_board/app/src/gpio/gpio.cpp
+++ firmware/c_board/app/src/gpio/gpio.cpp
@@ -2,8 +2,8 @@
 
 #include <cstdint>
 
+#include <gpio.h>
 #include <main.h>
-#include <stm32f4xx_hal_gpio.h>
 
 #include "firmware/c_board/app/src/spi/bmi088/accel.hpp"
 #include "firmware/c_board/app/src/spi/bmi088/gyro.hpp"
